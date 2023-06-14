c451033efc0b81d154d43f2e4ad79f80c35ea8ec
yejianbin233 <yejianbin233@163.com>
2023-06-14 09:52:26 +0800

    project init

---
+++ Source/MaterialsProject/MaterialsProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MaterialsProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MATERIALSPROJECT_API AMaterialsProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
